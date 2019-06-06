1c939dd84cd10d88e7d222ac398926d37a6857bd
Dawid Dyderski <dawiddyderski@MacBook-Pro-Dawid.local>
2019-06-06 11:57:22 +0200

    Extend Movie API endpoint with genre filter

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -3,11 +3,17 @@ class Api::V1::MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
-    render json: @movies
+    render json: @movies, include: :genre, filter_genre: filter_genre
   end
 
   def show
     @movie = Movie.find(params[:id])
-    render json: @movie
+    render json: @movie, include: :genre, filter_genre: filter_genre
+  end
+
+  private
+
+  def filter_genre
+    params[:filter] && params[:filter][:genre] == "true"
   end
 end
--- spec/controllers/api/v1/movies_controller_spec.rb
+++ spec/controllers/api/v1/movies_controller_spec.rb
@@ -4,7 +4,7 @@ describe Api::V1::MoviesController do
   let(:genre) { create(:genre, id: 3, name: "Action") }
   let!(:movie_godfather) { create(:movie, id: 1, title: "Godfather", genre: genre) }
   let!(:movie_deadpool) { create(:movie, id: 2, title: "Deadpool", genre: genre) }
-  let(:godfather_json) {
+  let(:godfather_without_filter_json) {
     {
       id: "1",
       type: "movies",
@@ -14,7 +14,7 @@ describe Api::V1::MoviesController do
     }
   }
 
-  let(:deadpool_json) {
+  let(:deadpool_without_filter_json) {
     {
       id: "2",
       type: "movies",
@@ -23,26 +23,99 @@ describe Api::V1::MoviesController do
       }
     }
   }
+  let(:godfather_with_filter_json) {
+    {
+      id: "1",
+      type: "movies",
+      attributes: {
+        title: "Godfather"
+      },
+      relationships: {
+        genre: {
+          data: {
+            id: "3",
+            type: "genres"
+          }
+        }
+      }
+    }
+  }
+
+  let(:deadpool_with_filter_json) {
+    {
+      id: "2",
+      type: "movies",
+      attributes: {
+        title: "Deadpool"
+      },
+      relationships: {
+        genre: {
+          data: {
+            id: "3",
+            type: "genres"
+          }
+        }
+      }
+    }
+  }
+  let(:genre_json) {
+    {
+      id: "3",
+      type: "genres",
+      attributes: {
+        name: "Action"
+      },
+    }
+  }
+  let(:relationships) { {} }
 
   describe "GET #index" do
-    it "displays all movies" do
-      get :index
+    context "without genre" do
+      it "displays all movies" do
+        get :index
 
-      expected_response = {
-        data: [godfather_json, deadpool_json]
-      }
-      expect(response.body).to eq(expected_response.to_json)
+        expected_response = {
+          data: [godfather_without_filter_json, deadpool_without_filter_json]
+        }
+        expect(response.body).to eq(expected_response.to_json)
+      end
+    end
+
+    context "with genre" do
+      it "displays all movies" do
+        get :index, params: { "filter[genre]": true }
+
+        expected_response = {
+          data: [godfather_with_filter_json, deadpool_with_filter_json],
+          included: [genre_json]
+        }
+        expect(response.body).to eq(expected_response.to_json)
+      end
     end
   end
 
   describe "GET #show" do
-    it "displays proper movie" do
-      get :show, params: { id: 2 }
+    context "without genre" do
+      it "displays proper movie" do
+        get :show, params: { id: 2 }
 
-      expected_response = {
-        data: deadpool_json
-      }
-      expect(response.body).to eq(expected_response.to_json)
+        expected_response = {
+          data: deadpool_without_filter_json
+        }
+        expect(response.body).to eq(expected_response.to_json)
+      end
+    end
+
+    context "with genre" do
+      it "displays proper movie" do
+        get :show, params: { id: 2, "filter[genre]": true }
+
+        expected_response = {
+          data: deadpool_with_filter_json,
+          included: [genre_json]
+        }
+        expect(response.body).to eq(expected_response.to_json)
+      end
     end
   end
 end
